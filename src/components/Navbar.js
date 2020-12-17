59cbd44d7a1813df4aedc54e05252baaf0916b3e
cristina <c.fujiname@gmail.com>
2020-12-17 09:18:12 +0000

    Updated Navbar component, fully tested

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
-import logo from '../logo.svg'
+import logo from '../logo.svg';
+import styled from 'styled-components';
+import { ButtonContainer } from './Button';
 
 export default class Navbar extends Component {
   render() {
     return (
-      <nav className='navbar navbar-expand-sm navbar-dark px-sm-5'>
+      <NavWrapper className='navbar navbar-expand-sm navbar-dark px-sm-5'>
         <Link to='/'>
           <img src={ logo } alt='lemon' className='navbar-brand' />
         </Link>
@@ -17,12 +19,23 @@ export default class Navbar extends Component {
           </li>
         </ul>
         <Link to='/cart' className='ml-auto'>
-          <button>
-            <i className='fas fa-cart-plus' />
-            my cart
-          </button>
+          <ButtonContainer>
+            <span className='mr-2'>
+              <i className='fas fa-cart-plus' />
+              my cart
+            </span>
+          </ButtonContainer>
         </Link>
-      </nav>
+      </NavWrapper>
     )
   }
 }
+
+const NavWrapper = styled.nav`
+  background: var(--mainWhite) ;
+  .nav-link {
+    color: var(--mainBlack) !important;
+    font-size: 2.3rem;
+    text-transform: capitalize;
+  }
+`
---
+++ src/test/Navbar.test.js
@@ -0,0 +1,20 @@
+import { render, screen } from '@testing-library/react';
+import App from '../App';
+
+test('renders the products link', () => {
+  render(<App />);
+  const linkElement = screen.getByText(/products/i);
+  expect(linkElement).toBeInTheDocument();
+});
+
+test('renders the my cart link', () => {
+  render(<App />);
+  const linkElement = screen.getByText(/my cart/i);
+  expect(linkElement).toBeInTheDocument();
+});
+
+test('renders the logo with a link', () => {
+  render(<App />);
+  const logoElement = screen.getByRole( 'link', { name: 'lemon'} )
+  expect(logoElement).toBeInTheDocument();
+});
